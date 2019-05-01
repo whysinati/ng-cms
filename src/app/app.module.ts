62ef365539540d879858d0bc827d2b1de3b4d2df
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-05-01 17:28:55 -0500

    implemented a user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { UsersService } from './users.service';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import { UsersComponent } from './users/users.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+//1. Import HTTP libs for API calls
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+//2. Import Observable 
+import { Observable } from 'rxjs';
+
+//3. Import the User object
+import { User } from './user';
+
+//3. Set outbound HTTP headers to JSON
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
 export class UsersService {
 
-  constructor() { }
+  //4. Set up the URL
+  private url:string = 'http://localhost:3000/api/users';
+
+  //5. Call the HttpClient in the Constructor
+  constructor(private http: HttpClient) { }
+
+  //6. Set up a simple observable.
+  getUsers(): Observable<User> {
+    return this.http.get<User>(this.url);
+  }
+
 }
