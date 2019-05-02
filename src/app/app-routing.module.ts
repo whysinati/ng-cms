41209fcf2a406e217fdbe5fdd4b79fbf807289d3
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-05-02 10:16:32 -0500

    generated and routed UserCreateComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent }   from './users/users.component';
 import { UserViewComponent }   from './user-view/user-view.component';
+import { UserCreateComponent }   from './user-create/user-create.component';
 
 // 3. Declare your routes
 const routes: Routes = [
@@ -9,7 +10,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   // 5. Map /users to the UsersComponent
   { path: 'users', component: UsersComponent },
-  { path: 'users/view/:id', component: UserViewComponent }
+  { path: 'users/view/:id', component: UserViewComponent },
+  { path: 'users/create', component: UserCreateComponent }
 ];
 
 @NgModule({
