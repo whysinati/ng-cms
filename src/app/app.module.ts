c01a9ace4d4ce1443e5a83cb9b518a86ca347a8b
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-05-02 10:17:35 -0500

    imported forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { UsersComponent } from './users/users.component';
 import { UsersService } from './users.service';
 import { UserViewComponent } from './user-view/user-view.component';
 import { UserCreateComponent } from './user-create/user-create.component';
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { UserCreateComponent } from './user-create/user-create.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [UsersService],
   bootstrap: [AppComponent]
